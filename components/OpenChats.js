587ebd589dd0340d40e35e67c1fda562ea490fba
Heath Dunlop <heathdunlop@Heaths-iMac.local>
2019-12-16 20:35:00 +1300

    ff

--- components/OpenChats.js
+++ components/OpenChats.js
@@ -77,10 +77,6 @@
 //           }
 //         ),
 //         onPanResponderGrant: (e, gestureState) => {
-//           pan.setOffset({
-//             x: 0,
-//             y: 0
-//           });
 //           // this.animatedValue.setOffset({
 //           //   x: this._value.x,
 //           //   y: this._value.y
@@ -414,110 +410,209 @@
  * KIND OF WORKING IN A FUNCTIONAL COMPOENNT
  */
 
-import React from "react";
+// import React from "react";
+// import {
+//   StyleSheet,
+//   View,
+//   Text,
+//   Dimensions,
+//   Animated,
+//   PanResponder
+// } from "react-native";
+
+// export default function Drag() {
+//   const dropZoneValues = React.useRef(null);
+//   const pan = React.useRef(new Animated.ValueXY());
+//   const [bgColor, setBgColor] = React.useState("#2c3e50");
+
+//   const isDropZone = React.useCallback(gesture => {
+//     const dz = dropZoneValues.current;
+//     return gesture.moveY > dz.y && gesture.moveY < dz.y + dz.height;
+//   }, []);
+
+//   const onMove = React.useCallback(
+//     (_, gesture) => {
+//       if (isDropZone(gesture)) setBgColor("red");
+//       else setBgColor("#2c3e50");
+//     },
+//     [isDropZone]
+//   );
+
+//   const setDropZoneValues = React.useCallback(event => {
+//     dropZoneValues.current = event.nativeEvent.layout;
+//   });
+
+//   const panResponder = React.useMemo(
+//     () =>
+//       PanResponder.create({
+//         onStartShouldSetPanResponder: () => true,
+
+//         onPanResponderMove: Animated.event(
+//           [
+//             null,
+//             {
+//               dx: pan.current.x,
+//               dy: pan.current.y
+//             }
+//           ],
+//           {
+//             listener: onMove
+//           }
+//         ),
+//         onPanResponderGrant: (evt, gesture) => {
+//           console.log("onPanResponderGrant")
+//           console.log("gesture => ", gesture)          
+
+//         },
+//         // onPanResponderMove: (evt, gesture) => {
+//         //   // Animated.event([{y: pan.current.y}])({y: gesture.moveY})
+//         //   Animated.event([
+//         //     null, {
+//         //       dx: pan.current.x,
+//         //       dy: pan.current.y
+//         //     }
+//         //   ], {
+//         //     listener: onMove
+//         //   })
+//         // },
+//         onPanResponderRelease: (e, gesture) => {
+//           console.log("onPanResponderRelease")
+//           console.log("gesture => ", gesture)
+//           console.log("current pan")
+//           console.log("gesture => ", pan.current) 
+//           // if (!isDropZone(gesture)) {
+//           //   Animated.spring(pan.current, { toValue: { x: 0, y: 0 } }).start();
+//           // }
+//           if (!isDropZone(gesture)) {
+//             Animated.spring(pan.current, { toValue: { x: gesture.moveX, y: gesture.moveY } }).start();
+//           }
+//           //on Release we need to update the point. it has a new offset and values now
+//         }
+//       }),
+//     []
+//   );
+
+//   return (
+//     <View style={styles.mainContainer}>
+//       <View
+//         onLayout={setDropZoneValues}
+//         style={[styles.dropZone, { backgroundColor: bgColor }]}
+//       >
+//         <Text style={styles.text}>Drop me here!</Text>
+//       </View>
+//       <View style={styles.draggableContainer}>
+//         <Animated.View
+//           {...panResponder.panHandlers}
+//           style={[pan.current.getLayout(), styles.circle]}
+//           // style={[ {top: pan.current.getLayout().top},styles.circle]}
+//         >
+//           <Text style={styles.text}>Drag me!</Text>
+//         </Animated.View>
+//       </View>
+//     </View>
+//   );
+// }
+
+// let CIRCLE_RADIUS = 36;
+// let Window = Dimensions.get("window");
+// let styles = StyleSheet.create({
+//   mainContainer: {
+//     flex: 1
+//   },
+//   dropZone: {
+//     height: 100,
+//     backgroundColor: "#2c3e50"
+//   },
+//   text: {
+//     marginTop: 25,
+//     marginLeft: 5,
+//     marginRight: 5,
+//     textAlign: "center",
+//     color: "#fff"
+//   },
+//   draggableContainer: {
+//     position: "absolute",
+//     top: Window.height / 2 - CIRCLE_RADIUS,
+//     left: Window.width / 2 - CIRCLE_RADIUS
+//   },
+//   circle: {
+//     backgroundColor: "#1abc9c",
+//     width: CIRCLE_RADIUS * 2,
+//     height: CIRCLE_RADIUS * 2,
+//     borderRadius: CIRCLE_RADIUS
+//   }
+// });
+
+
+import React, { Component } from "react";
 import {
+  AppRegistry,
   StyleSheet,
-  View,
   Text,
-  Dimensions,
+  View,
   Animated,
   PanResponder
 } from "react-native";
 
-export default function Drag() {
-  const dropZoneValues = React.useRef(null);
-  const pan = React.useRef(new Animated.ValueXY());
-  const [bgColor, setBgColor] = React.useState("#2c3e50");
+const  animatedbasic = () =>  {
+  const animatedValue = new Animated.ValueXY();
+    let _value = { x: 0, y: 0 };
+    animatedValue.addListener(value => (_value = value));
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+      onPanResponderGrant: (e, gestureState) => {
+        animatedValue.setOffset({
+          x: _value.x,
+          y: _value.y
+        });
+        animatedValue.setValue({ x: 0, y: 0 });
+      },
+      onPanResponderMove: Animated.event([
+        null,
+        { dx: animatedValue.x, dy: animatedValue.y }
+      ]),
+      onPanResponderRelease: (e, gestureState) => {
+        animatedValue.flattenOffset();
+        Animated.decay(animatedValue, {
+          deceleration: 0.997,
+          velocity: { x: gestureState.vx, y: gestureState.vy }
+        }).start();
+      }
+    });
 
-  const isDropZone = React.useCallback(gesture => {
-    const dz = dropZoneValues.current;
-    return gesture.moveY > dz.y && gesture.moveY < dz.y + dz.height;
-  }, []);
-
-  const onMove = React.useCallback(
-    (_, gesture) => {
-      if (isDropZone(gesture)) setBgColor("red");
-      else setBgColor("#2c3e50");
-    },
-    [isDropZone]
-  );
-
-  const setDropZoneValues = React.useCallback(event => {
-    dropZoneValues.current = event.nativeEvent.layout;
-  });
-
-  const panResponder = React.useMemo(
-    () =>
-      PanResponder.create({
-        onStartShouldSetPanResponder: () => true,
-
-        onPanResponderMove: Animated.event(
-          [
-            null,
-            {
-              dx: pan.current.x,
-              dy: pan.current.y
-            }
-          ],
-          {
-            listener: onMove
-          }
-        ),
-        onPanResponderRelease: (e, gesture) => {
-          if (!isDropZone(gesture)) {
-            Animated.spring(pan.current, { toValue: { x: 0, y: 0 } }).start();
-          }
-        }
-      }),
-    []
-  );
-
-  return (
-    <View style={styles.mainContainer}>
-      <View
-        onLayout={setDropZoneValues}
-        style={[styles.dropZone, { backgroundColor: bgColor }]}
-      >
-        <Text style={styles.text}>Drop me here!</Text>
-      </View>
-      <View style={styles.draggableContainer}>
+    const animatedStyle = {
+      transform: animatedValue.getTranslateTransform()
+    };
+    return (
+      <View style={styles.container}>
         <Animated.View
+          style={[styles.box, animatedStyle]}
           {...panResponder.panHandlers}
-          style={[pan.current.getLayout(), styles.circle]}
         >
-          <Text style={styles.text}>Drag me!</Text>
+          <Text style={styles.text}>Drag Me</Text>
         </Animated.View>
       </View>
-    </View>
-  );
+    );
 }
 
-let CIRCLE_RADIUS = 36;
-let Window = Dimensions.get("window");
-let styles = StyleSheet.create({
-  mainContainer: {
-    flex: 1
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
   },
-  dropZone: {
-    height: 100,
-    backgroundColor: "#2c3e50"
+  box: {
+    width: 150,
+    height: 150,
+    backgroundColor: "#333",
+    alignItems: "center",
+    justifyContent: "center"
   },
   text: {
-    marginTop: 25,
-    marginLeft: 5,
-    marginRight: 5,
-    textAlign: "center",
-    color: "#fff"
-  },
-  draggableContainer: {
-    position: "absolute",
-    top: Window.height / 2 - CIRCLE_RADIUS,
-    left: Window.width / 2 - CIRCLE_RADIUS
-  },
-  circle: {
-    backgroundColor: "#1abc9c",
-    width: CIRCLE_RADIUS * 2,
-    height: CIRCLE_RADIUS * 2,
-    borderRadius: CIRCLE_RADIUS
+    color: "#FFF",
+    fontSize: 20
   }
 });
+
+export default animatedbasic
